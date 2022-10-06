26efca36ebe932893dd8a95bc6106dcb09c48fd4
Just van Rossum <justvanrossum@gmail.com>
2022-10-06 09:36:27 +0200

    Add icns for macOS

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@
 set -e  # make sure to abort on error
 set -x  # echo commands
 
-pyinstaller FontraPakMain.py --windowed -y --collect-all fontra --collect-all fontra_rcjk --name "Fontra Pak"
+icon=$1
+
+pyinstaller FontraPakMain.py \
+	--windowed \
+	-y \
+	--collect-all fontra \
+	--collect-all fontra_rcjk \
+	--name "Fontra Pak" \
+	--osx-bundle-identifier xyz.fontra.fontra-pak \
+	--icon $icon
