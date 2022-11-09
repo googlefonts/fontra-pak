aca9b40239766c392b052e1671af576846efb99b
Just van Rossum <justvanrossum@gmail.com>
2022-11-09 20:00:41 +0100

    Run startup test with pytest

---
+++ tests/test_startup.py
@@ -0,0 +1,26 @@
+import pathlib
+import sys
+import subprocess
+
+
+repoRoot = pathlib.Path(__file__).resolve().parent.parent
+
+
+def test_startup():
+    if sys.platform == "darwin":
+        app_path = (
+            repoRoot / "dist" / "Fontra Pak.app" / "Contents" / "MacOS" / "Fontra Pak"
+        )
+    elif sys.platform == "win32":
+        app_path = repoRoot / "dist" / "Fontra Pak.exe"
+    else:
+        return
+    result = subprocess.run(
+        [app_path, "test-startup"],
+        capture_output=True,
+        timeout=15,
+        check=True,
+        encoding="utf-8",
+    )
+    assert "test-startup\n" == result.stdout
+    assert "" == result.stderr
