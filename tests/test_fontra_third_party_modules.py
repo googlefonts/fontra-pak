b4881e49383b7ce19099ff55ca72f871cf0b9d8b
Just van Rossum <justvanrossum@gmail.com>
2025-03-04 11:52:27 +0100

    Adjust test

--- tests/test_fontra_third_party_modules.py
+++ tests/test_fontra_third_party_modules.py
@@ -3,5 +3,5 @@ from importlib.resources import files
 
 def test_fontra_third_party_modules():
     # Make sure the third-party folder exists and isn't empty
-    names = list(files("fontra.client.third-party").iterdir())
+    names = list(files("fontra.client").iterdir())
     assert names
